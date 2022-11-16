155953b73d5dca172e7ffe48fbe11a85d30aff1d
YJK <jerryko103@gmail.com>
2022-11-17 00:29:32 +0800

    add DP

---
+++ Contents/section1/Dynamic Programming/Complete_Knapsack_problems.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int f[1000]={0};
+int n=0, m=0;
+int main(){
+    cin >> n >> m;
+    for (int i=1;i<=n;i++){
+        int price=0, value=0;
+        cin >> price >> value;
+        for (int j=price; j<=m; j++){
+            if (f[j-price]+value>f[j]){
+                f[j]=f[j-price]+value;
+            }
+        }
+    }
+    cout << f[m] << endl;
+    return 0;
+}
